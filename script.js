0e2b3fc765d122d95feef98fe0df7cee5a3afe8a
Bagas <91589286+bgas1212@users.noreply.github.com>
2023-11-07 14:54:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// uang jajan untuk sekolah anak pemalas
+
+// siswa memasukan berapa jumlah uang jajan yang di berikan
+// apabila siswa tidak diberikan uang jajan, maka siswa tidak dapat ke seko9lah
+// apabila siswa mendapat uang jajan, siswa dapat pergi ke sekolah
+// selesai
+
+let uang = parseInt(prompt("masukan uang jajan"));
+
+
+if (uang <= 0 ) {
+    alert ("kamu tidak sekolah")
+}else{
+    console.log("kamu sekolah");
+}
